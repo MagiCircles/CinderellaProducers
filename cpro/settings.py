7846a274b281c80d65fc6ca35ea61d74e45e97db
db0company <db0company@gmail.com>
2016-10-01 00:42:07 -0700

    Change static files version

--- cpro/settings.py
+++ cpro/settings.py
@@ -29,7 +29,7 @@ USER_COLORS = models.TYPES
 
 GET_GLOBAL_CONTEXT = utils.globalContext
 
-STATIC_FILES_VERSION = '9'
+STATIC_FILES_VERSION = '10'
 
 TOTAL_DONATORS = getattr(django_settings, 'TOTAL_DONATORS', 2) + 2
 FAVORITE_CHARACTERS = getattr(django_settings, 'FAVORITE_CHARACTERS', None)
